f6e6376013f8c75552d0a7621ebfb9467d47fcd6
Fatima Zohra Filali <filalifz7@gmail.com>
2025-08-21 20:51:07 -0400

    Initial commit

---
+++ src/main/java/com/zahra7/mymood/MyMoodApplication.java
@@ -0,0 +1,13 @@
+package com.zahra7.mymood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zahra7/mymood/MyMoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zahra7.mymood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
